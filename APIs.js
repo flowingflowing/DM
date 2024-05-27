723f2c13d11f45f86532bdbd2037b6ff4ec4ffce
flowingflowing <142156200+flowingflowing@users.noreply.github.com>
2024-05-28 00:55:13 +0900

    Add files via upload

---
+++ APIs.js
@@ -0,0 +1,11 @@
+import { TODO_KEY } from "./APIkey.js";
+
+const APIKEY = TODO_KEY;
+const USERNAME = "HongGildong";
+const HEADERS = {
+  "content-type": "application/json",
+  APIKEY: APIKEY,
+  username: USERNAME,
+};
+
+const APIURL = "https://example.com/api/todos";
